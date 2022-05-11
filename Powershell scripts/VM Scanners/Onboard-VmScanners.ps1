d64ff2742a81247fb5b7ac426498fa06d04718e9
eitanmechtinger <62880343+eitanmechtinger@users.noreply.github.com>
2022-05-11 12:26:53 +0300

    Update Onboard-VmScanners.ps1

--- Powershell scripts/VM Scanners/Onboard-VmScanners.ps1
+++ Powershell scripts/VM Scanners/Onboard-VmScanners.ps1
@@ -1,9 +1,15 @@
 param(
-[Parameter(Mandatory = $True)] [string]$targetRgName,
-[Parameter(Mandatory = $False)] [hashtable]$exclusionTags=@{}
+[Parameter(Mandatory = $False)] $exclusionTags=@{}
 )
 
-$objectid = (Get-AzADServicePrincipal -DisplayName "Microsoft Defender for Cloud Servers Scanner Resource Provider").id
+$objectid= (az ad sp list --display-name "Microsoft Defender for Cloud Servers Scanner Resource Provider" --query [].objectId --output tsv)
+$exclusionTagsJson = ($exclusionTags | ConvertTo-Json -Compress).Replace('"', '\"')
 
-Register-AzResourceProvider -ProviderNamespace Microsoft.Security
-New-AzDeployment -Location "West Europe" -targetRgName $targetRgName -exclusionTags $exclusionTags -principalId $objectid -TemplateUri uriuriuri
+$vmScannerRgName = "VmScannerResourceGroup"
+$vmScannerRgLocation = "westeurope"
+az group create -l $vmScannerRgLocation -n $vmScannerRgName
+
+az feature register --namespace Microsoft.Security --name VmScanners.Preview
+az provider register --namespace "Microsoft.Security" --wait
+
+az deployment sub create --location "West Europe" --template-uri uriuriuriuriuriuri --parameters vmScannerRgName=$vmScannerRgName principalId=$objectid exclusionTags=$exclusionTagsJson vmScannerRgLocation=$vmScannerRgLocation 
